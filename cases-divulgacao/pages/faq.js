86695dc2212422ff137e342ec0ab368d1d3ca178
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-02-28 07:28:33 -0300

    created pages

---
+++ cases-divulgacao/pages/faq.js
@@ -0,0 +1,10 @@
+import Link from '../src/components/Link'
+
+export default function FAQPage() {
+    return (
+        <div>
+            <h1>Cases - Página de Perguntas FAQ</h1>
+            <Link href="/">Ir para o home</Link>            
+        </div>
+    )
+}
