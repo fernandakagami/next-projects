ff2cf5a494867d4c860823bd03f5f1bbf3f474b6
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-02-28 08:22:16 -0300

    created styles

--- cases-divulgacao/pages/index.js
+++ cases-divulgacao/pages/index.js
@@ -1,12 +1,36 @@
+import HomeScreen from "../src/screens/HomeScreen"
+
+export default HomeScreen
+
+
+/*
+import React from 'react';
 import Link from '../src/components/Link'
+import Footer from '../src/components/patterns/Footer';
+
+function Title({ children, as }) {
+    const Tag = as
+    return (
+        <React.Fragment>
+            <Tag>{children}</Tag>
+            <style jsx>{`
+                ${Tag} {
+                    color: red;                    
+                }
+            `}</style>
+        </React.Fragment>
+    )
+}
 
 export default function HomePage() {
     return (
-        <div>
-            <h1>Cases - Home Page</h1>
+        <div> 
+            <Title as="h1">Cases - Home Page</Title>
             <Link href="/faq">
                 Ir para a página do FAQ
             </Link>
+            <Footer />
         </div>
     )
 }
+*/
